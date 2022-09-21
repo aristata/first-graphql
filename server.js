6fd4404b8804c9a39e266092515155b75aead326
장성민 <aristatait@gmail.com>
2022-09-21 14:42:03 +0900

    feat: scalar and root type

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import { ApolloServer, gql } from "apollo-server";
-
-// 우리 data 의 shape 를 graphql 한테 설명해줘야 한다
-// 반드시 Query 타입이 하나 이상 있어야만 한다
+/**
+ * 1. 우리 data 의 shape 를 graphql 한테 설명해줘야 한다
+ * 2. 반드시 Query 타입이 하나 있어야만 한다
+ * 3. 하위 타입들은 내가 정의해서 쓰면된다
+ * 4. argument 를 사용하여 조건을 구체화 할 수 있다
+ */
 const typeDefs = gql`
   type Query {
+    allTweets: [Tweet]
+    Tweet(id: ID): Tweet
+  }
+
+  type Tweet {
+    id: ID
     text: String
-    hello: String
+    author: User
+  }
+
+  type User {
+    id: ID
+    username: String
   }
 `;
 
