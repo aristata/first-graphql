8edf5d2e2671977ef384ca8e59642b0321bde67d
장성민 <aristatait@gmail.com>
2022-09-21 11:05:52 +0900

    feat: query type

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({});
+// 우리 data 의 shape 를 graphql 한테 설명해줘야 한다
+// 반드시 Query 타입이 하나 이상 있어야만 한다
+const typeDefs = gql`
+  type Query {
+    text: String
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
